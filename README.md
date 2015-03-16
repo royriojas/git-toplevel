0d6f06fb30a03ef4bd184bce2f9b8a35ebda32fe
Roy Riojas <roy@kno.com>
2015-03-16 01:32:42 -0700

    FEAT: first working version

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # git-toplevel
-Method to find the top level git directory
+> Method to find the top level git directory
+
+# Install
+```bash
+npm i --save git-toplevel
+```
+
+# Usage
+```javascript
+var gitTopLevel = require('git-toplevel');
+
+gitTopLevel().then(function(dir) {
+  // dir ===> the git root 
+}).catch(function(err) {
+  // err ===> any error that happen when trying to get the top level dir
+});
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = function ( cb ) {
+  var exec = require( 'child_process' ).exec;
+  var Promise = require( 'es6-promise' ).Promise;
+  var trim = require( 'lodash.trim' );
+
+  var p = new Promise( function ( resolve, reject ) {
+    exec( 'git rev-parse --show-toplevel', function ( err, stdout ) {
+      if ( err ) {
+        reject( err );
+        cb && cb( err );
+        return;
+      }
+      var val = trim( stdout );
+      resolve( val );
+      cb && cb( null, val );
+    } );
+  } );
+
+  return p;
+};
